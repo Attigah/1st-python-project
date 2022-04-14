a80d2d752579a49fd9dfbfe985e139c495961a32
Attigah <Attigahbernard6@gmail.com>
2022-04-14 01:14:08 +0000

    Update proj1.py

--- proj1.py
+++ proj1.py
@@ -11,4 +11,23 @@ answer = input("What does FTP stand for? ")
 if answer == "file transfer protocol":
     print('correct')
 else:
-    print('Incorrect, try again')
+    print('Incorrect, try again.')
+
+
+answer = input("What is the full meaning of cpu? ")
+if answer == "control processing unit":
+    print("correct")
+else:
+    print("incorrect, try again")
+
+answer = input("What is the full meaning of cpu? ")
+if answer == "control processing unit":
+    print("correct")
+else:
+    print("incorrect, try again")
+
+answer = input("What is the full meaning of cpu? ")
+if answer == "control processing unit":
+    print("correct")
+else:
+    print("incorrect, try again")
