fbcfd57481a90bfca426e303e41c0d07eabde0c4
Attigah <Attigahbernard6@gmail.com>
2022-04-14 23:08:00 +0000

    Update proj2.py

--- proj2.py
+++ proj2.py
@@ -1,3 +1,19 @@
 import random
 
-random.randrange(-25, 26)
+yourRandnum = random.randrange(-10, 26)
+
+highest_range = input("Type a number: ")
+if highest_range.isdigit():
+    highest_range = int(highest_range)
+
+    if highest_range <= 0:
+        print("please type a number higher than 0 ")
+        quit()
+
+
+else:
+    print("please type a number next time ")
+    quit()
+
+
+
