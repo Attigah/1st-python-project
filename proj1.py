59a24157be2234e98aadfe148242b48d048762ba
Attigah <Attigahbernard6@gmail.com>
2022-04-14 01:06:35 +0000

    Update proj1.py

--- proj1.py
+++ proj1.py
@@ -6,3 +6,9 @@ if playing != "yes":
     quit()
 
 print("Okay! Make we play :)")
+
+answer = input("What does FTP stand for? ")
+if answer == "file transfer protocol":
+    print('correct')
+else:
+    print('Incorrect, try again')
